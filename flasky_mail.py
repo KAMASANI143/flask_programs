db1e8b5d029e9d87d4dbf7524319d6a7084267a8
KAMASANI143 <81296996+KAMASANI143@users.noreply.github.com>
2021-03-24 17:35:25 +0530

    Update flasky_mail.py

--- flasky_mail.py
+++ flasky_mail.py
@@ -7,7 +7,7 @@ mail = Mail(app) # instantiate the mail class
 # configuration of mail
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'naveenknkn7@gmail.com'
+app.config['MAIL_USERNAME'] = ''
 app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
